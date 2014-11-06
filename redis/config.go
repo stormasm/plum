b89a206e8e19f1d1d9a57f371ce2fbdc10ecf21c
Michael Angerman <stormasm@gmail.com>
2014-11-06 09:29:58 -0800

    initial redis package up and running

---
+++ redis/config.go
@@ -0,0 +1,20 @@
+package redis
+
+import (
+	"path"
+)
+
+type ServerConfig struct {
+	Name         string
+	DiscoveryURL string
+	DataDir      string
+	SnapCount    uint64
+}
+
+func (c *ServerConfig) WALDir() string { return path.Join(c.DataDir, "wal") }
+
+func (c *ServerConfig) SnapDir() string { return path.Join(c.DataDir, "snap") }
+
+func (c *ServerConfig) ShouldDiscover() bool {
+	return c.DiscoveryURL != ""
+}
