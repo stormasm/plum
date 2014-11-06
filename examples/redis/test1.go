b259cacf22931deaacc5656ee1c2303fdc832941
Michael Angerman <stormasm@gmail.com>
2014-11-06 14:54:47 -0800

    redis config up and running with connect string

---
+++ examples/redis/test1.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+import "github.com/garyburd/redigo/redis"
+import "github.com/stormasm/plum/redisc"
+
+func main() {
+cfg := redisc.NewRedisConfig()
+connect_string := cfg.Connect_string()
+c, err := redis.Dial("tcp", connect_string)
+if err != nil {
+panic(err)
+}
+defer c.Close()
+
+//set
+c.Do("SET", "michael", "angerman")
+
+//get
+world, err := redis.String(c.Do("GET", "michael"))
+if err != nil {
+fmt.Println("key not found")
+}
+
+fmt.Println(world)
+//ENDINIT OMIT
+}
--- redisc/config.go
+++ redisc/config.go
@@ -1,25 +1,24 @@
 package redisc
 
+import (
+	"fmt"
+)
+
 type RedisConfig struct {
-	Db_zero        uint64
-	Db_uuid        uint64
-	Db_ap          uint64
-	Db_start       uint64
-	Key_db_next    string
-	Key_db_mapping string
-	Hostname_port  string
+	Hostname  string
+	Port      string
 }
 
-func (c *RedisConfig) db_zero() uint64 { return 0 }
+func (c *RedisConfig) Connect_string() string {
+	connect := fmt.Sprint(c.Hostname,":",c.Port)
+	return connect
+}
 
-func (c *RedisConfig) db_uuid() uint64 { return 10 }
-
-func (c *RedisConfig) db_ap() uint64 { return 11 }
-
-func (c *RedisConfig) db_start() uint64 { return 100 }
-
-func (c *RedisConfig) key_db_next() string { return "nextdb" }
-
-func (c *RedisConfig) key_db_mapping() string { return "hm:accountid:db" }
-
-func (c *RedisConfig) hostname_port() string { return ":6379" }
+func NewRedisConfig() (*RedisConfig) {
+	cfg := &RedisConfig{
+		Hostname:  "localhost",
+		Port:      "6379",
+	}
+	fmt.Printf("Initializing RedisConfig\n")
+	return cfg
+}
