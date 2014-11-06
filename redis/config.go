081de0b75510ae288ea134d7d6ed467e7a267321
Michael Angerman <stormasm@gmail.com>
2014-11-06 12:14:06 -0800

    update config

--- redis/config.go
+++ redis/config.go
@@ -1,20 +1,25 @@
 package redis
 
-import (
-	"path"
-)
-
-type ServerConfig struct {
-	Name         string
-	DiscoveryURL string
-	DataDir      string
-	SnapCount    uint64
+type RedisConfig struct {
+	Db_zero        uint64
+	Db_uuid        uint64
+	Db_ap          uint64
+	Db_start       uint64
+	Key_db_next    string
+	Key_db_mapping string
+	Hostname_port  string
 }
 
-func (c *ServerConfig) WALDir() string { return path.Join(c.DataDir, "wal") }
+func (c *RedisConfig) db_zero() uint64 { return 0 }
 
-func (c *ServerConfig) SnapDir() string { return path.Join(c.DataDir, "snap") }
+func (c *RedisConfig) db_uuid() uint64 { return 10 }
 
-func (c *ServerConfig) ShouldDiscover() bool {
-	return c.DiscoveryURL != ""
-}
+func (c *RedisConfig) db_ap() uint64 { return 11 }
+
+func (c *RedisConfig) db_start() uint64 { return 100 }
+
+func (c *RedisConfig) key_db_next() string { return "nextdb" }
+
+func (c *RedisConfig) key_db_mapping() string { return "hm:accountid:db" }
+
+func (c *RedisConfig) hostname_port() string { return ":6379" }
