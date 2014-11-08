fe80eaea9b51fbf962a2740401e49ccb9b75185b
Michael Angerman <stormasm@gmail.com>
2014-11-07 20:36:55 -0800

    intermediate snapshot

--- examples/redis/test3.go
+++ examples/redis/test3.go
@@ -4,6 +4,12 @@ import "fmt"
 import "github.com/stormasm/plum/redisc"
 
 func main() {
-  dbnumber := redisc.GetDbNumber_from_accountid("3")
+  //dbnumber := redisc.GetDbNumber_from_accountid("3")
+  //fmt.Println(dbnumber)
+
+  //nextvalue := redisc.AddOneToString("1010")
+  //fmt.Println(nextvalue)
+
+  dbnumber := redisc.CreateDbNumber_from_accountid("4")
   fmt.Println(dbnumber)
 }
--- redisc/token.go
+++ redisc/token.go
@@ -1,6 +1,7 @@
 package redisc
 
 import "fmt"
+import "strconv"
 import "github.com/garyburd/redigo/redis"
 
 func GetDbNumber_from_accountid(account string) string {
@@ -22,4 +23,68 @@ func GetDbNumber_from_accountid(account string) string {
 	}
 
 	return dbnumber
+}
+
+func AddOneToString(value string) string {
+	myint, err := strconv.Atoi(value)
+	if err != nil {
+		fmt.Println("problem converting string to integer")
+		return "-1"
+	}
+	myint_plus1 := myint + 1
+	plus1 := strconv.Itoa(myint_plus1)
+	return plus1
+}
+
+
+func CreateDbNumber_from_accountid(account string) string {
+	// First make sure it does not exist
+	if GetDbNumber_from_accountid(account) == "-1" {
+
+		cfg := NewRedisConfig()
+		connect_string := cfg.Connect_string()
+		c, err := redis.Dial("tcp", connect_string)
+		if err != nil {
+			panic(err)
+		}
+		defer c.Close()
+
+		tokencfg := NewTokenConfig()
+		redis.String(c.Do("SELECT", tokencfg.Db_ap))
+
+		nextdb, err := redis.String(c.Do("GET", tokencfg.Key_db_next))
+
+		fmt.Println(nextdb)
+
+		if err != nil {
+			return "-1"
+		}
+
+		/*
+		if nextdb == nil {
+			fmt.Println("nextdb does not exist")
+			dbnumber := tokencfg.Db_start
+			next_dbnumber := dbnumber
+			// convert string to int
+			next_dbnumber := strconv.Atoi(next_dbnumber)
+			// add 1 to the number
+			next_dbnumber := next_dbnumber + 1
+			// convert back to a string
+			next_dbnumber := strconv.Itoa(next_dbnumber)
+			// store the string in redis
+			redis.String(c.Do("SET", tokencfg.Key_db_next, next_db_number))
+		}
+		*/
+		//else {
+
+			dbnumber := nextdb
+			fmt.Println("nextdb already exists adding 1 to it")
+			plus1 := AddOneToString(dbnumber)
+			fmt.Println("nextdb = ", plus1)
+			// store it in redis
+			redis.String(c.Do("SET", tokencfg.Key_db_next, plus1))
+		//}
+	}
+	return "OK"
+	//return dbnumber
 }
