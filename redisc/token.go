4fff72eafb923b64099e0daa97f8881e52fdaa5d
Michael Angerman <stormasm@gmail.com>
2014-11-07 21:45:35 -0800

    code clean up

--- redisc/token.go
+++ redisc/token.go
@@ -18,7 +18,6 @@ func GetDbNumber_from_accountid(account string) string {
 	dbnumber, err := redis.String(c.Do("HGET", tokencfg.Key_db_mapping, account))
 
 	if err != nil {
-		fmt.Println("dbnumber not found", err)
 		return "-1"
 	}
 
@@ -39,12 +38,11 @@ func AddOneToString(value string) string {
 
 func CreateDbNumber_from_accountid(account string) string {
 	dbnumber := GetDbNumber_from_accountid(account)
-	// First make sure it does not exist
 	if dbnumber == "-1" {
-
 		cfg := NewRedisConfig()
 		connect_string := cfg.Connect_string()
 		c, err := redis.Dial("tcp", connect_string)
+
 		if err != nil {
 			panic(err)
 		}
@@ -52,32 +50,19 @@ func CreateDbNumber_from_accountid(account string) string {
 
 		tokencfg := NewTokenConfig()
 		redis.String(c.Do("SELECT", tokencfg.Db_ap))
-
 		nextdb, err := redis.String(c.Do("GET", tokencfg.Key_db_next))
 
-		fmt.Println(nextdb)
-
 		if err != nil {
-			fmt.Println("nextdb does not exist", err)
-
 			dbnumber = tokencfg.Db_start
 			plus1 := AddOneToString(dbnumber)
-			fmt.Println("nextdb = ", plus1)
-			// store it in redis
 			redis.String(c.Do("SET", tokencfg.Key_db_next, plus1))
-
 		}	else {
 			dbnumber = nextdb
-			fmt.Println("nextdb already exists adding 1 to it")
 			plus1 := AddOneToString(dbnumber)
-			fmt.Println("nextdb = ", plus1)
-			// store it in redis
 			redis.String(c.Do("SET", tokencfg.Key_db_next, plus1))
 		}
-		fmt.Println("Before")
+
 		redis.String(c.Do("HSET", tokencfg.Key_db_mapping, account, dbnumber))
-		fmt.Println("After")
-		//@redisc.hset(@key_db_mapping,account,db_number.to_s)
 	}
 	return dbnumber
 }
