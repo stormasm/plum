1a8f7f77ce030fe9302311a14fe29733930cac0a
Michael Angerman <stormasm@gmail.com>
2014-11-12 12:42:55 -0800

    move binding into plum for now, eventually once everything is working, we can grab the external repository

---
+++ examples/binding/test1.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "github.com/stormasm/plum/binding"
+)
+
+type ContactForm struct {
+    Email   string `json:"email"`
+    Message string `json:"message"`
+}
+
+// Then provide a field mapping (pointer receiver is vital)
+func (cf *ContactForm) FieldMap() binding.FieldMap {
+    return binding.FieldMap{
+        &cf.Email:   "email",
+        &cf.Message: "message",
+    }
+}
+
+// Now your handlers can stay clean and simple.
+func handler(resp http.ResponseWriter, req *http.Request) {
+    contactForm := new(ContactForm)
+    errs := binding.Bind(req, contactForm)
+    if errs.Handle(resp) {
+        return
+    }
+    fmt.Println("email = ", contactForm.Email)
+    fmt.Fprintf(resp, "Mia Email:   %s\n", contactForm.Email)
+    fmt.Fprintf(resp, "Iris Message: %s\n", contactForm.Message)
+}
+
+func main() {
+    http.HandleFunc("/contact", handler)
+    fmt.Println("Listening on port 3000")
+    http.ListenAndServe(":3000", nil)
+}
