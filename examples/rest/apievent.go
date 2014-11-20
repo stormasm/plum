78b21d1ae0091631ee53be0c391d50dc4564794c
Michael Angerman <stormasm@gmail.com>
2014-11-19 19:34:08 -0800

    working on the client apievent

---
+++ examples/rest/apievent.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+  "fmt"
+  "github.com/stormasm/goreq"
+)
+
+type Event1 struct {
+    AccessToken  string `json:"access_token"`
+    Dimension string `json:"token"`
+    Key string `json:"account"`
+    Value string `json:"project"`
+    CreatedAt string `json:"created_at"`
+    Interval []string `json:"interval"`
+    Calculation []string `json:"calculation"`
+}
+
+func main() {
+
+  item1 := Event1{ AccessToken: "104a5866-b844-4186-9322-19cacdcec298",
+                  Dimension: "job-skills",
+                  Key: "ruby",
+                  Value: "18",
+                  CreatedAt: "2014-09-28 16:33:31 -0700",
+                  Interval: []string{"hours","weeks","months"},
+                  Calculation: []string{"count","sum","average","standard_deviation","linear_regression"},
+                  }
+
+  fmt.Println(item1)
+
+  res1, err1 := goreq.Request{
+      Method: "POST",
+      ContentType: "application/json",
+      Uri: "http://localhost:4567/api/1.0/event",
+      Body: item1,
+  }.Do()
+
+  fmt.Println(err1, res1)
+
+}
