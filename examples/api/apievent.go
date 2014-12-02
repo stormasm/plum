631d70257530dac90686bb1068b6fb42b3022edf
Michael Angerman <stormasm@gmail.com>
2014-12-02 11:05:06 -0800

    we are now passing in the token to apievent

--- examples/api/apievent.go
+++ examples/api/apievent.go
@@ -1,57 +1,61 @@
 package main
 
 import (
-  "fmt"
-  "github.com/stormasm/goreq"
+	"fmt"
+	"github.com/stormasm/goreq"
+	"net/url"
 )
 
 type Event1 struct {
-    AccessToken  string `json:"access_token"`
-    Dimension string `json:"dimension"`
-    Key string `json:"key"`
-    Value string `json:"value"`
-    CreatedAt string `json:"created_at"`
-    Interval []string `json:"interval"`
-    Calculation []string `json:"calculation"`
+	AccessToken string   `json:"access_token"`
+	Dimension   string   `json:"dimension"`
+	Key         string   `json:"key"`
+	Value       string   `json:"value"`
+	CreatedAt   string   `json:"created_at"`
+	Interval    []string `json:"interval"`
+	Calculation []string `json:"calculation"`
 }
 
 func main() {
 
-  item1 := Event1{ AccessToken: "104a5866-b844-4186-9322-19cacdcec297",
-                  Dimension: "job-skills",
-                  Key: "ruby",
-                  Value: "18",
-                  CreatedAt: "2014-09-28 16:33:31 -0700",
-                  Interval: []string{"hours","weeks","months"},
-                  Calculation: []string{"count","sum","average","standard_deviation","linear_regression"},
-                  }
+	item1 := Event1{AccessToken: "104a5866-b844-4186-9322-19cacdcec297",
+		Dimension:   "job-skills",
+		Key:         "ruby",
+		Value:       "18",
+		CreatedAt:   "2014-09-28 16:33:31 -0700",
+		Interval:    []string{"hours", "weeks", "months"},
+		Calculation: []string{"count", "sum", "average", "standard_deviation", "linear_regression"},
+	}
 
-  fmt.Println(item1)
+	fmt.Println(item1)
 
-  res1, err1 := goreq.Request{
-      Method: "POST",
-      ContentType: "application/json",
-      Uri: "http://localhost:4567/api/1.0/event",
-      Body: item1,
-  }.Do()
+	res1, err1 := goreq.Request{
+		Method:      "POST",
+		ContentType: "application/json",
+		Uri:         "http://localhost:4567/api/1.0/event",
+		Body:        item1,
+	}.Do()
 
-  fmt.Println(err1, res1)
+	fmt.Println(err1, res1)
 
-  res2, err2 := goreq.Request{
-    Method: "GET",
-    ContentType: "application/json",
-    Uri: "http://localhost:4567/api/1.0/event/job-skills/python",
-    //Body: item1,
-    }.Do()
+	item := url.Values{}
+	item.Set("access_token", "28037e39-456d-49e4-998a-17c48ce916aa")
 
-  fmt.Println(err2, res2)
+	res2, err2 := goreq.Request{
+		Method:      "GET",
+		ContentType: "application/json",
+		Uri:         "http://localhost:4567/api/1.0/event/job-skills/python",
+		QueryString: item,
+	}.Do()
 
-  res3, err3 := goreq.Request{
-      Method: "GET",
-      ContentType: "application/json",
-      Uri: "http://localhost:4567/api/1.0/event/job-skills/go/sum/months",
-      //Body: item1,
-      }.Do()
+	fmt.Println(err2, res2)
 
-    fmt.Println(err3, res3)
+	res3, err3 := goreq.Request{
+		Method:      "GET",
+		ContentType: "application/json",
+		Uri:         "http://localhost:4567/api/1.0/event/job-skills/go/sum/months",
+		QueryString: item,
+	}.Do()
+
+	fmt.Println(err3, res3)
 }
